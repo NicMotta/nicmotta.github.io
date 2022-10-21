c231e74b3ce437af0e293999f2f2c378330c0916
Nic Motta <34142261+NicMotta@users.noreply.github.com>
2022-10-21 19:51:30 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nicmotta.github.io',
+  base: '/',
   integrations: [tailwind()]
 });
