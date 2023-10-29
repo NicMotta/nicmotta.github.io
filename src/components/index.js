786d0805983549f6c23089a32e712f058dc7d859
Nic Motta <nicolas.motta.contractor@gdevtools.com>
2023-10-29 17:52:44 -0300

    new component and barrel

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Card } from './Card.astro';
+export { default as ExhibitionList } from './ExhibitionList.astro';
+export { default as Footer } from './Footer.astro';
+export { default as FooterButton } from './FooterButton.astro';
+export { default as Header } from './Header.astro';
+export { default as HeaderButton } from './HeaderButton.astro';
+export { default as Image } from './Image.astro';
+export { default as Link } from './Link.astro';
+export { default as PortfolioCard } from './PortfolioCard.astro';
+export { default as Video } from './Video.astro';
