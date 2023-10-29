5cb077be84f5753918e59a1605fa2610166f2a0c
Nic Motta <nicolas.motta.contractor@gdevtools.com>
2023-10-29 17:53:02 -0300

    renamed constants and proyects

---
+++ src/constants/exhibitions.js
@@ -0,0 +1,167 @@
+export const EXHIBITIONS = [
+  {
+    year: 2023,
+    list: [
+      {
+        title: 'Festival BIOPOETRONIKA',
+        subtitle: 'Teatrín de la Municipalidad de Arequipa',
+        location: 'Arequipa, Perú'
+      },
+      {
+        title: 'Coexistencias',
+        subtitle: 'MUNTREF Tecnópolis',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Respiro Namiki en la Semana Internacional del diseño',
+        subtitle: 'Universidad de Palermo',
+        location: 'Buenos Aires, Argentina'
+      }
+    ]
+  },
+  {
+    year: 2022,
+    list: [
+      {
+        title: 'Residencia LIC - Laboratorio de Indicadores Culturales',
+        subtitle: 'El Túnel',
+        location: 'Entre Ríos, Argentina'
+      }
+    ]
+  },
+  {
+    year: 2021,
+    list: [
+      {
+        title: 'Bienal de la Habana',
+        subtitle: 'Museo Wilfredo Lam',
+        location: 'La Habana, Cuba'
+      },
+      {
+        title: 'Premio FUSIÓN 2020',
+        subtitle: 'Centro Cultural San Martín',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Simposio de Arte Sonoro',
+        subtitle: 'Universidad Nacional Tres de Febrero',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: '16º Simposio de Arte Contemporáneo',
+        subtitle: 'Universidad Federal de Santa María',
+        location: 'Santa María, Brasil'
+      },
+      {
+        title: 'FACTORS 8.0',
+        subtitle: 'Universidad Federal de Santa María',
+        location: 'Santa María, Brasil'
+      },
+      {
+        title: 'Proyecto BIOS',
+        subtitle: 'Performance colectiva en Mozilla Hub',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Proyecto BIOS',
+        subtitle: 'Objeto A',
+        location: 'Buenos Aires, Argentina'
+      }
+    ]
+  },
+  {
+    year: 2020,
+    list: [
+      {
+        title: 'Territorio / #ProaADistancia',
+        subtitle: 'Fundación PROA',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Del dato a la materia',
+        subtitle: 'Universidad Nacional de las Artes',
+        location: 'Cátedra Cáceres'
+      }
+    ]
+  },
+  {
+    year: 2019,
+    list: [
+      {
+        title: 'data_mesh',
+        subtitle: 'Espacio Huellas',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Bienalsur - Invadir/Resistir',
+        subtitle: 'Centro Cultural de España en Costa Rica',
+        location: 'San José, Costa Rica'
+      },
+      {
+        title: 'Festival PROYECTOR',
+        subtitle: 'Quinta del Sordo',
+        location: 'Madrid, España'
+      },
+      {
+        title: 'Festival Estéticas Expandidas',
+        subtitle: 'Universidad de Nariño',
+        location: 'Nariño, Colombia'
+      },
+      {
+        title: 'Bienalsur - Invadir/Resistir',
+        subtitle: 'Museo de Arquitectura',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Ingresando datos, procesando emergencias',
+        subtitle: 'Universidad Museo Social Argentino',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Wunderkammer: Objetos Utópicos',
+        subtitle: 'Universidad Complutense',
+        location: 'Madrid, España'
+      }
+    ],
+  },
+  {
+    year: 2018,
+    list: [
+      {
+        title: 'Noviembre Electrónico - Noche de los Museos',
+        subtitle: 'Universidad Museo Social Argentino',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Antología Bienal Kosice',
+        subtitle: 'Centro Cultural San Martín',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'Festival Transversal',
+        subtitle: 'Instituto Baikal',
+        location: 'Buenos Aires, Argentina'
+      },
+      {
+        title: 'FACTORS - Festival de Arte, Ciencia y Tecnología',
+        subtitle: 'Universidad Federal de Santa María',
+        location: 'Santa María, Brasil'
+      },
+      {
+        title: '1º Premio ARCITEC - Arte Electrónico',
+        subtitle: 'Centro Cultural San Martín',
+        location: 'Buenos Aires, Argentina'
+      }
+    ]
+  },
+  {
+    year: 2017,
+    list: [
+      {
+        title: 'FASE9 - Noviembre Electrónico',
+        subtitle: 'Centro Cultural San Martín',
+        location: 'Buenos Aires, Argentina'
+      }
+    ]
+  }
+];
